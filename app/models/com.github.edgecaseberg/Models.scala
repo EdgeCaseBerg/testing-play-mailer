4d26dce02a952196661690ea8938c1a9f7a0f150
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-12-07 20:57:11 -0500

    Add documentation to Emailable

--- app/models/com.github.edgecaseberg/Models.scala
+++ app/models/com.github.edgecaseberg/Models.scala
@@ -6,18 +6,17 @@ trait Emailable {
 	def email: InternetAddress
 }
 
-/** Helper Companion to assist with Creating InternetAddress's 
+/** Helper Companion to assist with Creating InternetAddress's
  *
- * {{{
- * import Emailable._
- * User("username", "user@localhost")	// Will automatically convert 
- * User("username", "") // Will throw AddressException
- * }}}
+ *  {{{
+ *  import Emailable._
+ *  User("username", "user@localhost")	// Will automatically convert
+ *  User("username", "") // Will throw AddressException
+ *  }}}
  */
 object Emailable {
 	implicit def string2Email(str: String): InternetAddress = new InternetAddress(str, true)
 }
-
 
 /** Dummy Class just to have an example */
 case class User(val username: String, val email: InternetAddress) extends Emailable
