2a9d503310e94d1234719e190b3de2891ae9192b
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-12-07 21:35:04 -0500

    Add welcome templates and MailService

---
+++ app/services/com.github.edgecaseberg/MailService.scala
@@ -0,0 +1,38 @@
+package com.github.edgecaseberg.services
+
+import com.github.edgecaseberg.models._
+import com.typesafe.plugin.{ MailerAPI, MailerPlugin, _ }
+
+import play.api.Play.current
+import play.api.libs.concurrent.Akka
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+
+class MailService(implicit ec: ExecutionContext) {
+	val fromAddress = "tester@domain.com"
+
+	def sendWelcomeEmailTo(to: Emailable) {
+		val subject = "Welcome!"
+		val (name, emailable) = to match {
+			case User(username, email) => (username, email)
+			case BillableClient(name, _, email) => (name, email)
+			case _ => ("", to.email)
+		}
+
+		val sendTo = emailable.getAddress()
+		val plainText: String = views.txt.email.welcome(name, fromAddress).body
+		val htmlText: String = views.html.email.welcome(name, fromAddress).body
+		sendMail(fromAddress, sendTo, subject, plainText, htmlText)
+	}
+
+	def sendMail(from: String, to: String, subject: String, plainTextBody: String, htmlBody: String): Unit = Akka.system.scheduler.scheduleOnce(1.second) {
+		val email: MailerAPI = use[MailerPlugin].email
+
+		email.setRecipient(to)
+		email.setFrom(from)
+		email.setSubject(subject)
+
+		email.send(plainTextBody, htmlBody)
+	}
+}
