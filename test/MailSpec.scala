9199d7e546b04115334088b2c90aca28cec11383
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-12-07 23:48:08 -0500

    Clearing Mailboxes before tests

--- test/MailSpec.scala
+++ test/MailSpec.scala
@@ -2,12 +2,15 @@ package test
 
 import org.scalatest._
 import com.icegreen.greenmail.util.{ GreenMail, ServerSetup }
+import scala.collection.JavaConverters._
+
+case class SimpleEmail(to: String, from: String, subject: String, plain: String, html: String)
 
 /** Specification for Testing an SMTP Server.
  *
  *  @note Should be mixed in last since it extends BeforeAndAfterAll
  */
-trait MailSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
+trait MailSpec extends FlatSpec with Matchers with BeforeAndAfterEach with BeforeAndAfterAll {
 	/** The current mailPort for this test
 	 *
 	 *  @note A def so that it can be overridden by users of the test
@@ -26,6 +29,18 @@ trait MailSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
 	override def afterAll() {
 		super.afterAll()
 		greenMail.stop();
+	}
+
+	override def beforeEach() {
+		clearMailboxes
+		super.beforeEach()
+	}
+
+	def clearMailboxes = {
+		val managers = greenMail.getManagers()
+		val users = managers.getUserManager().listUser().asScala
+		val imapManager = managers.getImapHostManager()
+		users.map(imapManager.getInbox(_)).map(_.deleteAllMessages())
 	}
 
 }
