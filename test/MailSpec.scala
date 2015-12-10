3fe6a3f989aa946370fb879760a7d35c5d1370df
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-12-09 21:08:23 -0500

    Add utility methods to MailSpec
    In order to make our tests simple we parse out the basics of what we
    want (in general) from an email. Some good reference material is here:
    
    Multipart Format: http://www.freesoft.org/CIE/RFC/1521/18.htm
    Docs: https://javamail.java.net/nonav/docs/api/javax/mail/internet/MimeMessage.html
    Useful Forum: http://www.coderanch.com/t/597373/java/java/Body-text-javamail-retrieve-email

--- test/MailSpec.scala
+++ test/MailSpec.scala
@@ -4,7 +4,10 @@ import org.scalatest._
 import com.icegreen.greenmail.util.{ GreenMail, ServerSetup }
 import scala.collection.JavaConverters._
 
-case class SimpleEmail(to: String, from: String, subject: String, plain: String, html: String)
+import javax.mail.internet.MimeMessage
+import javax.mail._
+
+case class SimpleEmail(to: List[String], from: List[String], subject: String, plain: Option[String], html: Option[String])
 
 /** Specification for Testing an SMTP Server.
  *
@@ -41,6 +44,63 @@ trait MailSpec extends FlatSpec with Matchers with BeforeAndAfterEach with Befor
 		val users = managers.getUserManager().listUser().asScala
 		val imapManager = managers.getImapHostManager()
 		users.map(imapManager.getInbox(_)).map(_.deleteAllMessages())
+	}
+
+	def waitForInbox(milliseconds: Long) {
+		greenMail.waitForIncomingEmail(milliseconds, 1)
+	}
+
+	def getMessageForEmail(email: String): List[SimpleEmail] = {
+		val user = greenMail.setUser(email, null)
+		val inbox = greenMail.getManagers().getImapHostManager().getInbox(user)
+		inbox.getMessages().asScala.sortWith(
+			(l, r) => l.getReceivedDate().after(r.getReceivedDate())
+		).map(msg => mimeMessageToSimpleEmail(msg.getMimeMessage())).toList
+	}
+
+	def getTextFromBodyPart(p: javax.mail.Part): Option[String] = {
+		if (p.isMimeType("text/*")) {
+			Some(p.getContent().asInstanceOf[String])
+		} else {
+			None
+		}
+	}
+
+	def arr2List[T](arr: Array[T]): List[T] = {
+		arr.toList
+	}
+
+	def mimeMessageToSimpleEmail(mimeMsg: MimeMessage): SimpleEmail = {
+		val subject = mimeMsg.getSubject()
+		val senders = arr2List(mimeMsg.getReplyTo()).map(_.toString())
+		val recipients = arr2List(mimeMsg.getAllRecipients()).map(_.toString())
+		/* Now the annoying part. The Message itself and all the disposition and such */
+		val mailParts = mimeMsg.getContent().asInstanceOf[Multipart]
+		var plainText = Option[String](null)
+		var htmlText = Option[String](null)
+		for (p <- 0 to mailParts.getCount()) {
+			/* Code adapted from studying javadocs and http://www.coderanch.com/t/597373/java/java/Body-text-javamail-retrieve-email */
+			val bodyPart = mailParts.getBodyPart(p)
+			if (bodyPart.isMimeType("text/*")) {
+				if (bodyPart.isMimeType("text/html")) {
+					htmlText = getTextFromBodyPart(bodyPart)
+				} else {
+					plainText = getTextFromBodyPart(bodyPart)
+				}
+			} else if (bodyPart.isMimeType("multipart/*")) {
+				val multiPart = bodyPart.getContent().asInstanceOf[Multipart]
+				for (mp <- 0 to multiPart.getCount()) {
+					val part = multiPart.getBodyPart(mp)
+					if (part.isMimeType("text/plain")) {
+						plainText = getTextFromBodyPart(part)
+					} else if (part.isMimeType("text/html")) {
+						htmlText = getTextFromBodyPart(part)
+					}
+				}
+			}
+		}
+
+		SimpleEmail(recipients, senders, subject, plainText, htmlText)
 	}
 
 }
